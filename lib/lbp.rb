b63a5bbb44fe0ff40720c07f4d2c87cb5bda28e8
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-02-12 20:23:27 -0500

    fixed old reference to item group in lib/lbp.rb

--- lib/lbp.rb
+++ lib/lbp.rb
@@ -5,8 +5,7 @@ require "lbp/version"
   require 'lbp/item'
   require 'lbp/paragraph'
   require 'lbp/transcription'
-	require 'lbp/item_group'
-  require 'lbp/collection'
+	require 'lbp/collection'
   require 'lbp/query'
   
 
