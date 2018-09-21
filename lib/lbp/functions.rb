377471300a63d3b34a8db3fee741d1e58bea2a20
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2018-09-21 08:28:40 -0400

    updated functions to close open-uri object for xslt file

--- lib/lbp/functions.rb
+++ lib/lbp/functions.rb
@@ -4,18 +4,18 @@ require 'open-uri'
 
 def xslt_transform(xml_open_uri_file, xsltfile, xslt_param_array)
 	xml  = xml_open_uri_file
-	xslt = Nokogiri::XSLT(open(xsltfile))
+	xsltfile = open(xsltfile)
+	xslt = Nokogiri::XSLT(xsltfile)
+	xsltfile.close
 	result_doc = xslt.transform(xml, xslt_param_array)
 	return result_doc
 end
 
 def xslt_apply_to(xml_open_uri_file, xsltfile, xslt_param_array)
 	xml  = xml_open_uri_file
+	xsltfile = open(xsltfile)
 	xslt = Nokogiri::XSLT(open(xsltfile))
+	xsltfile.close
 	result_doc = xslt.apply_to(xml, xslt_param_array)
 	return result_doc
 end
-
-
-
-				
